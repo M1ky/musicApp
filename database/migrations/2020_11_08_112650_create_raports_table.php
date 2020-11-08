5f0580c4a771f7b1a200b6a5eba9ecc4edbcef25
M1ky <michal.kuban2@gmail.com>
2020-11-08 21:02:53 +0100

    inserts

--- database/migrations/2020_11_08_112650_create_raports_table.php
+++ database/migrations/2020_11_08_112650_create_raports_table.php
@@ -16,6 +16,27 @@ class CreateRaportsTable extends Migration
             $table->timestamps();
         });
 
+        \Illuminate\Support\Facades\DB::table('reports')->insert(
+            array(
+                'id' => 1,
+                'title' => 'Report 1',
+                'year' => 1985,
+                'month' => 12,
+            ),
+            array(
+                'id' => 2,
+                'title' => 'Report 2',
+                'year' => 1986,
+                'month' => 1,
+            ),
+            array(
+                'id' => 3,
+                'title' => 'Report 3',
+                'year' => 1986,
+                'month' => 2,
+            )
+        );
+
         Schema::create('report_track', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('report_id');
@@ -26,6 +47,27 @@ class CreateRaportsTable extends Migration
             $table->foreign('track_id')->references('id')->on('tracks')->onDelete('cascade');
             $table->foreign('report_id')->references('id')->on('reports')->onDelete('cascade');
         });
+
+        \Illuminate\Support\Facades\DB::table('report_track')->insert(
+            array(
+                'id' => 1,
+                'report_id' => 1,
+                'track_id' => 1,
+                'broadcasts' => 50,
+            ),
+            array(
+                'id' => 2,
+                'report_id' => 1,
+                'track_id' => 2,
+                'broadcasts' => 45,
+            ),
+            array(
+                'id' => 3,
+                'report_id' => 1,
+                'track_id' => 2,
+                'broadcasts' => 65,
+            ),
+        );
     }
 
     public function down()
